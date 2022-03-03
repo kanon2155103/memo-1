90707c84c31dae6faa4223e4a981d17cbe276c92
Kanon Nakajima <kanon2155103@gmail.com>
2022-03-03 21:38:03 +0900

    モジュールの追加

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 const http = require('http');
+const router = require('./lib/router');
 const server = http.createServer((req, res) => {
-	res.writeHead(302, {
-		Location:'https://thawing-basin-10677.herokuapp.com/'
-	});
-	res.end();
+	router.route(req, res);
 })
 .on('error', e => {
 	console.error('Server Error', e);
